e9b184c8960431a9cdc544754798c83d53f8ac25
Lenovo X1 <maataouihamza53@gmail.com>
2024-01-15 09:23:08 +0100

    test commit 2

--- src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello it's me hamza :)";
+        return "Hello it's hhme hamza :)";
     }
 
     @GetMapping("/user")
